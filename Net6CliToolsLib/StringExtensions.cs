e670377d2ec1f782aefadbc7a67fe0f381f5b0c6
Brian Kneebone <77240212+brian-kneebone@users.noreply.github.com>
2022-02-12 12:53:18 -0500

    add check if argument possibility

---
+++ Net6CliToolsLib/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net6CliTools
+{
+    public static class StringExtensions
+    {
+        public static bool MightBeAnArgumentName(this string value)
+        {
+            if (value == null)
+                return false;
+
+            if (value.StartsWith("-") || value.StartsWith("--"))
+                return true;
+
+            return false;
+        }
+    }
+}
