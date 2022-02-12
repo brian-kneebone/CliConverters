2b68034d12f521e512cdce294bcfe6d54fc36b00
Brian Kneebone <77240212+brian-kneebone@users.noreply.github.com>
2022-02-12 12:47:32 -0500

    refactor arguments to toolparameters with filename property

--- Net6CliToolsLib/ToolParametersBase.cs
+++ Net6CliToolsLib/ToolParametersBase.cs
@@ -7,39 +7,44 @@ using System.IO;
 
 namespace Net6CliTools
 {
-    public abstract class ArgumentsBase
+    public abstract class ToolParametersBase
     {
         public FileInfo? Filename { get; private set; } = null;
 
         protected string[] Args => this._args;
         private readonly string[] _args;
 
-        protected ArgumentsBase(string[] args)
+        protected ToolParametersBase(string[] args)
         {
             this._args = args ?? throw new ArgumentNullException(nameof(args));
             var remaining = this.ParseArguments();
-            this.ParseRemainingCommonArguments(remaining);
+            this.ParseRemainingArguments(remaining);
 
         }
 
         protected abstract string[] ParseArguments();
 
-        private void ParseRemainingCommonArguments(string[] args)
+        private void ParseRemainingArguments(string[] args)
         {
             if (args == null || args.Length == 0)
                 return;
 
-            if (args.Length == 1)
-                this.ParseFileName(args[0]);
+            this.ParseFileNameIfPresent(args);
+            
         }
 
-        private void ParseFileName(string filename)
+        private void ParseFileNameIfPresent(string[] args)
         {
-            this.Filename = new FileInfo(filename);
+            if (args.Length == 0)
+                return;
+
+            this.Filename = new FileInfo(args[1]);
 
             if (this.Filename.Exists)
-                throw new FileNotFoundException(nameof(filename));
+                throw new FileNotFoundException(args[1]);
+
         }
+
 
 
     }
