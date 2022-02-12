e0585c0d087d72e1ac92242bc56827e59e954371
Brian Kneebone <77240212+brian-kneebone@users.noreply.github.com>
2022-02-12 13:26:36 -0500

    fix null test handling

--- Net6CliToolsLibTest/StringExtensionTests.cs
+++ Net6CliToolsLibTest/StringExtensionTests.cs
@@ -13,9 +13,17 @@ namespace Net6CliTools.Test
         [TestMethod]
         public void MayBeArgumentNameTest()
         {
-            string? nullString = null;
 
-            Assert.IsFalse(nullString?.MightBeArgumentName());
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+#pragma warning disable CS8604 // Possible null reference argument.
+
+            string nullString = null;
+            var nullIsFalse = nullString.MightBeArgumentName();
+
+#pragma warning restore CS8604 // Possible null reference argument.
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+            Assert.IsFalse(nullIsFalse);
             Assert.IsFalse("sdfsdf".MightBeArgumentName());
             Assert.IsFalse("".MightBeArgumentName());
 
