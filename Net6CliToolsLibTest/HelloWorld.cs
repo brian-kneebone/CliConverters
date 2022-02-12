913bbc13eb8bfcadfa0d646811a1483aeef60f59
Brian Kneebone <77240212+brian-kneebone@users.noreply.github.com>
2022-02-12 13:03:46 -0500

    add unit test for tools lib

---
+++ Net6CliToolsLibTest/HelloWorld.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Net6CliTools.Test
+{
+    [TestClass]
+    public class HelloWorld
+    {
+        [TestMethod]
+        public void HelloWorldTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
