0933c72c853bb189a42a579a4aa8e6db05f8ad5d
Brian Kneebone <77240212+brian-kneebone@users.noreply.github.com>
2022-02-12 17:45:58 -0500

    add short and long name only test param

---
+++ Net6CliToolsLibTest/Parameters/ShortAndLongNameOnlyParameter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net6CliTools.Test.Parameters
+{
+    internal class ShortAndLongNameOnlyParameter : NameOnlyParameter
+    {
+        internal const string SHORT_NAME = "d";
+
+        internal const string LONG_NAME = "dummy-arg-name-d";
+
+        public override bool IsRequired => true;
+
+        private ShortAndLongNameOnlyParameter(string? shortName, string longName) : base(shortName, longName)
+        {
+        }
+
+
+    }
+}
