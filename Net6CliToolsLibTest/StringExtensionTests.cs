41f83be7db3740947ebd92d5f0f0aba633886e3d
Brian Kneebone <77240212+brian-kneebone@users.noreply.github.com>
2022-02-12 13:10:43 -0500

    test might be argument name

---
+++ Net6CliToolsLibTest/StringExtensionTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net6CliTools.Test
+{
+    [TestClass]
+    public class StringExtensionTests
+    {
+        [TestMethod]
+        public void MayBeArgumentNameTest()
+        {
+            string? nullString = null;
+
+            Assert.IsFalse(nullString?.MightBeAnArgumentName());
+            Assert.IsFalse("sdfsdf".MightBeAnArgumentName());
+            Assert.IsFalse("".MightBeAnArgumentName());
+
+            Assert.IsFalse("-".MightBeAnArgumentName());
+            Assert.IsFalse("--".MightBeAnArgumentName());
+
+            Assert.IsTrue("-arg1".MightBeAnArgumentName());
+            Assert.IsTrue("--arg1".MightBeAnArgumentName());
+            
+            Assert.IsFalse("---arg1".MightBeAnArgumentName());
+            Assert.IsFalse("-- -arg1".MightBeAnArgumentName());
+
+            Assert.IsTrue("-arg1".MightBeAnArgumentName());
+            Assert.IsTrue("--arg1".MightBeAnArgumentName());
+
+            Assert.IsFalse("- arg1".MightBeAnArgumentName());
+            Assert.IsFalse("-- arg1".MightBeAnArgumentName());
+
+            Assert.IsFalse("-arg1-sdfsdf".MightBeAnArgumentName());
+            Assert.IsTrue("--arg1-sdfsdf".MightBeAnArgumentName());
+            Assert.IsFalse("--arg1 sdfsdf".MightBeAnArgumentName());
+        }
+    }
+}
