d9a6feb2667ac443976d3f6e124f244f4f953164
Brian Kneebone <77240212+brian-kneebone@users.noreply.github.com>
2022-02-13 14:20:51 -0500

    add text logger test

---
+++ Net6CliToolsLibTest/Loggers/TestTextLogger.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net6CliTools.Loggers
+{
+    [TestClass]
+    public class TestTextLogger
+    {
+        [TestMethod]
+        public void TestQuicklyOpeningAndClosing()
+        {
+            var logger = TextFileLogger.Create("QuickOpenCloseTest", LogLevels.Debug);
+            logger.Info("Test quickly opening & closing.");
+            logger.Dispose();
+            Assert.IsTrue(logger.State == LoggerStates.Closed);
+        }
+    }
+}
