d2c1b1bb5d8c4135b1379a126ebb0fe5fcb61ab4
Brian Kneebone <77240212+brian-kneebone@users.noreply.github.com>
2022-02-12 23:30:03 -0500

    add demo name only parameter

---
+++ Net6CliToolsLibTest/Parameters/DemoNameOnlyParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net6CliTools.Parameters
+{
+    internal class DemoNameOnlyParameter : NameOnlyParameter
+    {
+        internal DemoNameOnlyParameter(string? shortName, string longName) : base(shortName, longName)
+        {
+
+        }
+    }
+}
