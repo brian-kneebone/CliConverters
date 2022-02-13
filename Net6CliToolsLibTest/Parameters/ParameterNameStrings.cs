0292c007119dfc37932047ac5d3de377b1a01b0d
Brian Kneebone <77240212+brian-kneebone@users.noreply.github.com>
2022-02-12 23:30:24 -0500

    add param name strings

---
+++ Net6CliToolsLibTest/Parameters/ParameterNameStrings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net6CliTools.Parameters
+{
+    internal static class ParameterNameStrings
+    {
+        internal const string NULL = null;
+        internal const string EMPTY = "";
+        internal const string TAB = "\t";
+        internal const string CRLF = "\r\n";
+        internal const string SPACE = " ";
+
+        internal const string SHORT_A = "a";
+        internal const string SHORT_B = "a";
+        internal const string SHORT_C = "c";
+        internal const string SHORT_D = "d";
+
+        internal const string LONG_A = "a-long";
+        internal const string LONG_B = "b-long";
+        internal const string LONG_C = "c-long";
+        internal const string LONG_D = "d-long";
+
+    }
+}
