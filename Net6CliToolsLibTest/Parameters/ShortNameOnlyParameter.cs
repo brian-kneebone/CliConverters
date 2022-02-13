cd26153f405dfdc3aa5ac6d8fadd66548fd43714
Brian Kneebone <77240212+brian-kneebone@users.noreply.github.com>
2022-02-12 20:34:42 -0500

    implement short name only param

---
+++ Net6CliToolsLibTest/Parameters/ShortNameOnlyParameter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net6CliTools.Parameters
+{
+    internal class ShortNameOnlyParameter : NameOnlyParameter
+    {
+        internal const string SHORT_NAME = "c";
+
+        internal const string LONG_NAME = null;
+
+        internal ShortNameOnlyParameter() : base(SHORT_NAME, LONG_NAME)
+        {
+        }
+
+    }
+}
