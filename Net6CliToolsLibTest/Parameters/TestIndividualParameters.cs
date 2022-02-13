d107e55296c79397bdf34387016ab3cb20cc356c
Brian Kneebone <77240212+brian-kneebone@users.noreply.github.com>
2022-02-12 22:02:07 -0500

    refactor name only test parameters

---
+++ Net6CliToolsLibTest/Parameters/TestIndividualParameters.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net6CliTools.Parameters
+{
+    [TestClass]
+    public class TestIndividualParameters
+    {
+        [TestMethod]
+        public void TestNameOnlyParameters()
+        {
+            #pragma warning disable CA1806
+            Assert.ThrowsException<ArgumentNullException>(new Action(() => { new EmptyNameOnlyParameter(); }));
+            Assert.ThrowsException<ArgumentNullException>(new Action(() => { new ShortNameOnlyParameter(); }));
+            #pragma warning restore CA1806
+
+            Assert.IsNotNull(new LongNameOnlyParameter());
+            Assert.IsNotNull(new ShortAndLongNameOnlyParameter());
+        }
+    }
+}
