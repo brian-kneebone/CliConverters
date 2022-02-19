a418af58a344c4d2f002dde42404b9d99035904d
Brian Kneebone <77240212+brian-kneebone@users.noreply.github.com>
2022-02-19 10:21:17 -0500

    add TextFileWriter multithreaded initial test (passing)

--- Net6CliToolsLibTest/Loggers/TestTextFileWriter.cs
+++ Net6CliToolsLibTest/Loggers/TestTextFileWriter.cs
@@ -38,5 +38,89 @@ namespace Net6CliTools.Loggers
             Assert.IsTrue(!File.Exists(filename));
         }
 
+        [TestMethod]
+        [ExcludeFromCodeCoverage]
+        public void TestMultithreadedWrite()
+        {
+            var filename = "TextFileWriter_MultithreadedWrite_" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss-fff") + ".log";
+            var writer = new TextFileWriter(filename);
+            writer.StartAsync();
+
+            var tasks = CreateThreadsThatErrorLog(writer, 10);
+            StartThreadsThatErrorLog(tasks);
+            WaitUntilThreadsThatErrorLogComplete(tasks, writer, 50);
+
+            writer.Stop();
+
+            Assert.IsTrue(tasks.Count(t => t.Status == TaskStatus.RanToCompletion) == tasks.Length);
+            // Assert.IsTrue(writer.State == LoggerStates.Closed);
+            Assert.IsTrue(File.Exists(filename));
+            Assert.IsTrue(File.ReadAllBytes(filename).Length > 0);
+        }
+
+        private static Task[] CreateThreadsThatErrorLog(TextFileWriter writer, int numberOfThreads)
+        {
+            var tasks = new Task[numberOfThreads];
+
+            for (int i = 0; i < numberOfThreads; i++)
+            {
+                tasks[i] = new Task(() => { InitiateRandomizedErrorLogging(writer, 10, 50); });
+            }
+
+            return tasks;
+        }
+
+        private static void StartThreadsThatErrorLog(Task[] tasks)
+        {
+            foreach (var task in tasks)
+                task.Start();
+        }
+
+        private static void WaitUntilThreadsThatErrorLogComplete(Task[] tasks, TextFileWriter writer, int waitBetweenStatusChecksInMs)
+        {
+            writer.WriteLineAsync("Start Monitoring");
+
+            var continueRunning = true;
+            var random = new Random();
+
+            while (continueRunning)
+            {
+                writer.WriteLineAsync(" -> Thread Status:");
+
+                var statusResults = tasks.Select(t => t.Status).ToArray();
+                var statusTypes = Enum.GetNames<TaskStatus>();
+
+                foreach (var statusType in statusTypes)
+                {
+                    var count = statusResults.Count(r => r.ToString() == statusType);
+                    writer.WriteLineAsync($" ---> {statusType}: {count}");
+                }
+
+                var numberDone = statusResults.Count(t => (t == TaskStatus.RanToCompletion) || (t == TaskStatus.Faulted) || (t == TaskStatus.Canceled));
+                continueRunning = tasks.Length != numberDone;
+
+                writer.WriteLineAsync($" -> Tasks Done: {numberDone} of {tasks.Length})");
+                writer.WriteLineAsync($" -> Continue Running: {continueRunning}");
+
+                if (continueRunning)
+                    System.Threading.Thread.Sleep(random.Next(0, waitBetweenStatusChecksInMs));
+
+            }
+
+            writer.WriteLineAsync("Stop Monitoring");
+
+        }
+
+        private static void InitiateRandomizedErrorLogging(TextFileWriter writer, int iterations, int maxWaitMs)
+        {
+            var threadId = Guid.NewGuid().ToString();
+            var random = new Random();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                writer.WriteLineAsync($"{threadId}: {i + 1}");
+                System.Threading.Thread.Sleep(random.Next(0, maxWaitMs));
+            }
+        }
     }
 }
