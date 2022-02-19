dfb1c4df776ce0fe3eed12cefa4cb1ed67ec9a26
Brian Kneebone <77240212+brian-kneebone@users.noreply.github.com>
2022-02-19 17:41:35 -0500

    add file info exists with wait extension

---
+++ Net6CliToolsLib/FileInfoExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net6CliTools
+{
+    public static class FileInfoExtensions
+    {
+        public static bool Exists(this FileInfo value, int timeoutInMilliseconds)
+        {
+            if (value.Exists)
+                return true;
+
+            var started = DateTime.Now;
+            var duration = TimeSpan.Zero;
+
+            while (duration.TotalMilliseconds < timeoutInMilliseconds)
+            {
+                if (value.Exists)
+                    return true;
+
+                duration = DateTime.Now - started;
+                Thread.Sleep(50);
+            }
+
+            return value.Exists;
+        }
+    }
+}
